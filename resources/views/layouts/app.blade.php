c68e93416ffe941f9f0f1c5c6284042feeb48076
agleis <amg295@cornell.edu>
2017-11-10 23:42:33 -0500

    Added countdown timer to coming soon.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -147,9 +147,45 @@
             text-align: center;
             font-size: 200%;
         }
+        h2 {
+          display: inline;
+        }
       </style>
       <img src="{{asset('../Logo.png')}}" />
-      <p>Coming soon.</p>
+      <h1>Coming 11/15.</h1>
+      <br />
+      <p id="demo"></p>
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Nov 15, 2017 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = "<h2>" + days + "</h2>" + " days " + "<h2>" + hours + "</h2>" + " hours "
+  + "<h2>" + minutes + "</h2>" + " minutes " + "<h2>" + seconds + "</h2>" + " seconds ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
     @endif
 
     <!-- Scripts -->
