5a3b0e5708b10e558c34942017c0aa51be51716e
agleis <amg295@cornell.edu>
2017-11-14 19:48:43 -0500

    Prepping for launch

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,12 +30,15 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js"></script>
 </head>
 <body>
-    @if(Auth::check() or request()->is('login') or request()->is('register'))
     <div id="app">
 
       <!-- FALL ISSUE BANNER -->
       @if(request()->is('/'))
-      <div id="header-svg"><a href="https://issuu.com/guacmag/docs/hidden_20gems" target="_blank"><img src="{{asset('images/header.svg')}}"></a></div>
+        <div id="header-svg">
+          <a href="https://issuu.com/guacmag/docs/hidden_20gems" target="_blank">
+            <img src="{{asset('images/header.svg')}}" />
+          </a>
+        </div>
       @endif
 
       <!-- toggle navbar-fixed-top class -->
@@ -158,52 +161,6 @@
             </div>
         </div>
     </div>
-    @else
-      <style>
-        body {
-            text-align: center;
-            font-size: 200%;
-        }
-        h2 {
-          display: inline;
-        }
-      </style>
-      <img src="{{asset('../Logo.png')}}" />
-      <h1>Coming 11/15.</h1>
-      <br />
-      <p id="demo"></p>
-
-<script>
-// Set the date we're counting down to
-var countDownDate = new Date("Nov 15, 2017 00:00:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get todays date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = "<h2>" + days + "</h2>" + " days " + "<h2>" + hours + "</h2>" + " hours "
-  + "<h2>" + minutes + "</h2>" + " minutes " + "<h2>" + seconds + "</h2>" + " seconds ";
-
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-</script>
-    @endif
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
